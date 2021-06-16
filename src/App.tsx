7ac06b02b92b9c70029f9859c3f3682d3f1865ba
nesrine mrabet <mrabet_nesrine@yahoo.fr>
2021-06-16 15:43:18 +0100

    Add types and map over data

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,29 @@
-import React, { useEffect } from "react";
+import React, { useState, useEffect } from "react";
 import "./App.css";
+import { Geolocation } from "./types/index";
 import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
 import pattern from "./assets/pattern-bg.png";
 import { ReactComponent as Arrow } from "./assets/icon-arrow.svg";
 
 function App() {
+  const [positions, setPositions] = useState<Geolocation[]>([]);
+
   const url =
     "https://geo.ipify.org/api/v1?apiKey=at_JLuFr4jNnjSoYLFCG5hxN11jzMNGB&ipAddress=41.224.222.185";
   useEffect(() => {
     fetch(url)
       .then((res) => res.json())
-      .then((data) => console.log(data));
-  }, []);
+      .then((data: Geolocation[]) => setPositions(data));
+  }, [url]);
+
+  console.log(positions);
 
   return (
     <div className="">
       <img
         src={pattern}
         alt=""
-        className="static z-20 h-1/3 object-cover md:w-full"
+        className="static z-20 h-1/3 md:h-2/3 object-cover md:w-full"
       />
       <div className="mx-auto z-50 absolute top-12 left-0 right-0 w-8/12">
         <h2 className="text-center text-lg font-medium mb-3 text-white">
@@ -37,38 +42,46 @@ function App() {
             <Arrow className="mx-auto outline-none" />
           </button>
         </div>
-        <div className="mx-auto text-center rounded-xl w-11/12 bg-white border m-6 lg:flex flex-row justify-between lg:w-8/12 p-6">
-          <div className="">
-            <h3 className="font-bold text-xs">IP ADDRESS</h3>
-          </div>
-          <div>
-            <h3 className="font-bold text-xs">LOCATION</h3>
-          </div>
-          <div>
-            <h3 className="font-bold text-xs">TIMEZONE</h3>
-          </div>
-          <div>
-            <h3 className="font-bold text-xs">ISP</h3>
-          </div>
-        </div>
+        {positions.map((place) => {
+          return (
+            <div className="mx-auto text-center rounded-xl w-11/12 bg-white border m-6 lg:flex flex-row justify-between lg:w-8/12 p-6">
+              <div className="" key={place.location.geonameId}>
+                <h3 className="font-bold text-xs">IP ADDRESS</h3>
+                <h2>{place.ip}</h2>
+              </div>
+              <div>
+                <h3 className="font-bold text-xs">LOCATION</h3>
+                <h2>{place.location.city}</h2>
+              </div>
+              <div>
+                <h3 className="font-bold text-xs">TIMEZONE</h3>
+                <h2>{place.location.timezone}</h2>
+              </div>
+              <div>
+                <h3 className="font-bold text-xs">ISP</h3>
+                <h2>{place.isp}</h2>
+              </div>
+            </div>
+          );
+        })}
       </div>
-      <div className=' relative z-10'>
+      <div className=" relative z-10">
         <MapContainer
-          center={[51.505, -0.09]}
+          center={[36.81897, 10.16579]}
           zoom={13}
           scrollWheelZoom={false}
-          style={{ height: "95vh", width: "100vw", overflow: "hidden" }}
         >
           <TileLayer
             attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
             url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
           />
-          <Marker position={[51.505, -0.09]}>
+          <Marker position={[36.81897, 10.16579]}>
             <Popup>
               A pretty CSS3 popup. <br /> Easily customizable.
             </Popup>
           </Marker>
         </MapContainer>
+        ,
       </div>
     </div>
   );
---
+++ src/types/index.d.ts
@@ -0,0 +1,27 @@
+export interface Geolocation {
+  ip: string;
+  location: {
+    country: string;
+    region: string;
+    city: string;
+    lat: number;
+    lng: number;
+    postalCode: string;
+    timezone: string;
+    geonameId: number;
+  };
+  domains: [string, string, string, string, string];
+  as: {
+    asn: number;
+    name: string;
+    route: string;
+    domain: string;
+    type: string;
+  };
+  isp: string;
+  proxy: {
+    proxy: boolean;
+    vpn: boolean;
+    tor: boolean;
+  };
+}
