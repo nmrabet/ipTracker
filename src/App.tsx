b74891312d04852084b2ed2729a4bdaf91996633
nesrine mrabet <mrabet_nesrine@yahoo.fr>
2021-06-18 20:23:28 +0100

    Add async await data

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
 import "./App.css";
 import { Geolocation } from "./types/index";
 import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
@@ -6,19 +6,26 @@ import pattern from "./assets/pattern-bg.png";
 import { ReactComponent as Arrow } from "./assets/icon-arrow.svg";
 
 function App() {
-  const [position, setPosition] = useState<Geolocation | null>();
+  const [position, setPosition] = useState<Geolocation>();
+  const [query, setQuery] = useState("");
 
-  const url =
-    "https://geo.ipify.org/api/v1?apiKey=at_JLuFr4jNnjSoYLFCG5hxN11jzMNGB&ipAddress=41.224.222.185";
+  const searchLocation = async (e) => {
+    e.preventDefault();
+    console.log("submitting");
 
-  useEffect(() => {
-    fetch(url)
-      .then((res) => res.json())
-      .then((data: Geolocation) => setPosition(data))
-      .then((err) => console.error(err));
-  }, []);
+    const url = `https://geo.ipify.org/api/v1?apiKey=at_JLuFr4jNnjSoYLFCG5hxN11jzMNGB&ipAddress=${query}`;
 
-  console.log(position);
+    try {
+      const res = await fetch(url);
+      const data = await res.json();
+      setPosition(data);
+    } catch (err) {
+      console.error(err);
+    }
+  };
+
+  let DEFAULT_LONGITUDE = position?.location.lng;
+  let DEFAULT_LATITUDE = position?.location.lat;
 
   return (
     <div>
@@ -31,10 +38,16 @@ function App() {
         <h2 className="text-center text-2xl font-medium mb-5 text-white">
           IP Address Tracker
         </h2>
-        <div className="text-center flex justify-center items-center ">
+        <form
+          className="text-center flex justify-center items-center"
+          onSubmit={searchLocation}
+        >
           <input
-            type="search"
+            type="text"
+            name="query"
             placeholder="Search for any IP address or domain"
+            value={query}
+            onChange={(e) => setQuery(e.target.value)}
             className="pl-2 w-8/12 mb-4 border rounded-tl-xl rounded-bl-xl h-10 outline-none"
           />
           <button
@@ -43,60 +56,61 @@ function App() {
           >
             <Arrow className="mx-auto outline-none" />
           </button>
+        </form>
+        <div>
+          {position && (
+            <div className="mx-auto space-y-4 text-center rounded-xl w-11/12 p-6  bg-white border m-6 md:text-left lg:p-14 lg:flex flex-row justify-between lg:space-x-6 lg:space-y-0 lg:w-11/12 ">
+              <div className="" key={position.location.geonameId}>
+                <h3
+                  className="font-medium text-xs tracking-wider"
+                  style={{ color: "#3F3F3F" }}
+                >
+                  IP ADDRESS
+                </h3>
+                <h2 className="mt-3 text-xl font-medium">{position.ip}</h2>
+              </div>
+              <div className="hidden lg:block lg:border"></div>
+              <div>
+                <h3
+                  className="font-medium text-xs tracking-wider"
+                  style={{ color: "#3F3F3F" }}
+                >
+                  LOCATION
+                </h3>
+                <h2 className="mt-3 text-xl font-medium">
+                  {position.location.city}
+                </h2>
+              </div>
+              <div className="hidden lg:block lg:border"></div>
+              <div>
+                <h3
+                  className="font-medium text-xs tracking-wider"
+                  style={{ color: "#3F3F3F" }}
+                >
+                  TIMEZONE
+                </h3>
+                <h2 className="mt-3 text-xl font-medium">
+                  {position.location.timezone}
+                </h2>
+              </div>
+              <div className="hidden lg:block lg:border"></div>
+              <div>
+                <h3
+                  className="font-medium text-xs tracking-wider"
+                  style={{ color: "#3F3F3F" }}
+                >
+                  ISP
+                </h3>
+                <h2 className="mt-3 text-xl font-medium">{position.isp}</h2>
+              </div>
+            </div>
+          )}
         </div>
-
-        {position && (
-          <div className="mx-auto space-y-4 text-center rounded-xl w-11/12 p-6  bg-white border m-6 md:text-left lg:p-14 lg:flex flex-row justify-between lg:space-x-6 lg:space-y-0 lg:w-11/12 ">
-            <div className="" key={position.location.geonameId}>
-              <h3
-                className="font-medium text-xs tracking-wider"
-                style={{ color: "#3F3F3F" }}
-              >
-                IP ADDRESS
-              </h3>
-              <h2 className="mt-3 text-xl font-medium">{position.ip}</h2>
-            </div>
-            <div className="hidden lg:block lg:border"></div>
-            <div>
-              <h3
-                className="font-medium text-xs tracking-wider"
-                style={{ color: "#3F3F3F" }}
-              >
-                LOCATION
-              </h3>
-              <h2 className="mt-3 text-xl font-medium">
-                {position.location.city}
-              </h2>
-            </div>
-            <div className="hidden lg:block lg:border"></div>
-            <div>
-              <h3
-                className="font-medium text-xs tracking-wider"
-                style={{ color: "#3F3F3F" }}
-              >
-                TIMEZONE
-              </h3>
-              <h2 className="mt-3 text-xl font-medium">
-                {position.location.timezone}
-              </h2>
-            </div>
-            <div className="hidden lg:block lg:border"></div>
-            <div>
-              <h3
-                className="font-medium text-xs tracking-wider"
-                style={{ color: "#3F3F3F" }}
-              >
-                ISP
-              </h3>
-              <h2 className="mt-3 text-xl font-medium">{position.isp}</h2>
-            </div>
-          </div>
-        )}
       </div>
 
       <div className=" relative z-10">
         <MapContainer
-          center={[36.81897, 10.16579]}
+          center={[DEFAULT_LONGITUDE, DEFAULT_LATITUDE]}
           zoom={13}
           scrollWheelZoom={false}
           style={{ height: "100vh" }}
@@ -105,13 +119,10 @@ function App() {
             attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
             url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
           />
-          <Marker position={[36.81897, 10.16579]}>
-            <Popup>
-              A pretty CSS3 popup. <br /> Easily customizable.
-            </Popup>
+          <Marker position={[DEFAULT_LONGITUDE, DEFAULT_LATITUDE]}>
+            <Popup>You are here !</Popup>
           </Marker>
         </MapContainer>
-        ,
       </div>
     </div>
   );
